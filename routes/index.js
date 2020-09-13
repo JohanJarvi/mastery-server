e99b5cd7255e6e60833fe1dcca39f3d36b0d66d6
Johan Jarvi <johan.jarvi@gmail.com>
2020-09-13 12:12:39 +1000

    MS-2: Send back basic server status message.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send("Connected - running.");
+  res.send({ status: "Connected - running." });
 });
 
 module.exports = router;
